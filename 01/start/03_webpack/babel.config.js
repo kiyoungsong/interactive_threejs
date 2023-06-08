73ef5cb54a8e4e3e43d57c393d719e0561c26b62
ky.song <thdrldud369@gmail.com>
2023-06-08 14:44:00 +0900

    ⚡ feat: 웹팩 설정

--- 01/start/03_webpack/babel.config.js
+++ 01/start/03_webpack/babel.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-	presets: [
-		['@babel/preset-env', {
-			targets: {
-				chrome: '58',
-				ie: '11',
-			},
-			useBuiltIns: 'usage',
-			corejs: {
-				version: 3,
-			}
-		}]
-	]
+  // 최신 문법으로 작성된 것을 과거 문법으로 변경해서 잘 구동되게 해줌
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          chrome: "58",
+          ie: "11",
+        },
+        useBuiltIns: "usage",
+        corejs: {
+          version: 3,
+        },
+      },
+    ],
+  ],
 };
